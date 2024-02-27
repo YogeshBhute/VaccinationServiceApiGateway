2bc64b0d165c03469027cb2ce55df6f2a4a1aef5
YogeshBhute <yog.bhute@gmail.com>
2024-02-27 21:59:55 +0530

    Initial Commit

---
+++ src/main/java/com/microservices/vaccination/VaccinationServiceApiGateway/VaccinationServiceApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.microservices.vaccination.VaccinationServiceApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationServiceApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationServiceApiGatewayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/microservices/vaccination/VaccinationServiceApiGateway/VaccinationServiceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.vaccination.VaccinationServiceApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationServiceApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
